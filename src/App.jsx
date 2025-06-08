fa0725a46b2dd4780f52ce4a0def001c55727936
GauravLamichhane <gaurav.lamichhane02@gmail.com>
2025-06-08 20:14:20 +0545

    Click events

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import List from "./List";
-function App() {
-  const fruits = [
-    {id:1,name:"Banana",calories:105},
-    {id:2,name:"Apple",calories:95},
-    {id:3,name:"Pineappel",calories:37},
-  ];
+import Button from "./Button";
 
-  const vegetables = [
-    {id:4,name:"Potatoes",calories:105},
-    {id:5,name:"Celery",calories:95},
-    {id:6,name:"Carrots",calories:37},
-  ];
+function App() {
   return(<>
-  {fruits.length > 0 ? <List items = {fruits} category = {"Fruits"}/> : null} 
-  {vegetables.length > 0 ? <List items = {vegetables} category = {"Vegetables"}/>: null}
-  </>
-);
+  <Button/>
+  </>);
 }
 
 export default App
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,9 +1,15 @@
-// import styles from './Button.module.css'
 function Button(){
+  let count = 0;
+  const handleClick = (name) => {
+    if(count < 3){
+      count++;
+      console.log(`${name} you clicked me ${count} times.`)
+    }
+    else{
+      console.log("Stop clicking me.");
+    }
+  } 
 
-
-
-  
-  return(<button className="Button">Click Me</button>)
+  return(<button onClick={()=>handleClick("gaurav")}>Click Me 👌</button>);
 }
 export default Button
